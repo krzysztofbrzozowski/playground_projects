7a6cde91e3e57e381252b222b2b7cf85c4ae3d9d
Krzysztof Brzozowski <krzysztof_brzozowski@icloud.com>
2023-02-07 23:25:41 +0100

    Added files for each algorithm

---
+++ algorithms/binary_search/binary_search.py
@@ -0,0 +1,7 @@
+"""
+@author:    Krzysztof Brzozowski
+@file:      0_binary_search
+@time:      07/02/2023
+@desc:      
+"""
+
---
+++ algorithms/binary_search/test_binary_search.py
@@ -0,0 +1,11 @@
+"""
+@author:    Krzysztof Brzozowski
+@file:      test_binary_search
+@time:      07/02/2023
+@desc:      Binary search algorithm shall look for requested value in log2(n) steps while linear search in n steps.
+"""
+import pytest
+
+
+def test():
+    pass
