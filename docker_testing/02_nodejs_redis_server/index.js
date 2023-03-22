f6f37a52e17748847eec15c9c56f2c4d71c5458e
Krzysztof Brzozowski <krzysztof_brzozowski@icloud.com>
2023-03-22 21:24:09 +0100

    Checking restarting policies

--- docker_testing/02_nodejs_redis_server/index.js
+++ docker_testing/02_nodejs_redis_server/index.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const redis = require('redis');
+const process = require('process');
 
 const app = express();
 // Since app is running in Docker, specifying redis container name is fine
 const client = redis.createClient({
   host: 'redis-server',
-  port: 6379
-
+  port: 6379,
 });
 client.set('visits', 0);
 
 app.get('/', (req, res) => {
   client.get('visits', (err, visits) => {
-    res.send('Number of visits is ' + visits);
+    // Testing purposes only
+    process.exit(0)
+    res.send('Number of visits ' + visits);
     client.set('visits', parseInt(visits) + 1);
   });
 });
